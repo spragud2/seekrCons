1ed3b3aa9acc895975776b264639f8d315cee24d
Daniel Sprague <danielsprague@mbl18.wireless-1x.unc.edu>
2019-05-07 10:42:41 -0400

    global phyloP

--- parse.py
+++ parse.py
@@ -1,73 +1,35 @@
 import pandas as pd
 import pickle
 import numpy as np
-
+from itertools import product
 import pyBigWig as bw
 from seekr.fasta_reader import Reader
 from collections import defaultdict
 
 import gtfparse
 
-phyloP = bw.open('../mm10.60way.phyloP60wayPlacental.bw')
+import seaborn as sns
+import matplotlib.pyplot as plt
+import multiprocessing
+import argparse
 
-mouseGTF = gtfparse.read_gtf('/Users/danielsprague/Downloads/gencode.vM21.annotation.gtf')
-mouseGTF['gene_id'] = [i[:-2] for i in mouseGTF['gene_id']]
-
-mouseGTF.head()
-
-hits = pickle.load(open('fixed_mm10_pctranscripts_4_99_250win_25slide_scores.p','rb'))
-
-df = pd.DataFrame.from_dict(hits,orient='index')
-
-df.head()
-
-query_dict = defaultdict(list)
-
-newHits = {}
-
-for row in df.iterrows():
-    query_dict = defaultdict(list)
-    name = row[0]
-    data = row[1:][0][0]
-    for i in data:
-        query_dict[i[1]].append(i[0])
-    hits[name] = query_dict.copy()
-    query_dict.clear()
-
-df_2 = pd.DataFrame.from_dict(hits,orient='index')
-
-qfa = Reader('./queries.fa')
-q_h = qfa.get_headers()
-
-q_map = dict(zip(list(range(13)),q_h))
-
-q_map
-
-df_2.rename(columns=q_map,inplace=True)
-
-from tqdm import tqdm_notebook as tqdm
-
-def dSeekrCoords(df):
+def dSeekrCoords(df,w,s):
     hitDict = {}
-    for row in tqdm(df.iterrows(),total=len(df)):
+    for row in df.iterrows():
         loc,data = row[0],row[1]
-        wtf = []
+        seekrGenomicCoords = []
         for idx,queryHitLocs in enumerate(data):
             if isinstance(queryHitLocs,list):
-                continuousCoords =  [np.arange(start=i*100,stop=i*100+1000,step=1) for i in queryHitLocs]
+                continuousCoords =  [np.arange(start=i*s,stop=i*s+w,step=1) for i in queryHitLocs]
                 mergedCoords = np.concatenate(continuousCoords)
                 flatCoords = np.unique(mergedCoords)
-                wtf.append(flatCoords)
-        hitDict[loc] = np.unique(np.concatenate(wtf))
+                seekrGenomicCoords.append(flatCoords)
+        hitDict[loc] = np.unique(np.concatenate(seekrGenomicCoords))
     return hitDict
 
-wtf = dSeekrCoords(df_2)
-
-seekrAgg=[]
-intronsAgg=[]
-exonsAgg=[]
-
-for info,coords in tqdm(wtf.items()):
+def getCons(sCoords,gtf):
+    info,coords = sCoords
+    print(info,coords)
     infoSplit = info.split('|')
     coordsArray = np.array(coords,dtype=int)
     gene,transcript,strand,start,end = infoSplit[0][1:],infoSplit[1],int(infoSplit[2]),int(infoSplit[3]),int(infoSplit[4])
@@ -97,18 +59,62 @@ for info,coords in tqdm(wtf.items()):
             intronsPhyloP = np.array([phyloP.values(chrom,start=i,end=i+1) for i in INTRONS])
         if (EXONS) and (len(EXONS)>1):
             exonsPhyloP = np.array([phyloP.values(chrom,start=i,end=i+1) for i in EXONS])
-        seekrAgg.append(np.median(seekrPhyloP))
-        intronsAgg.append(np.median(intronsPhyloP))
-        exonsAgg.append(np.median(exonsPhyloP))
+
+        return gene, (seekrPhyloP,intronsPhyloP,exonsPhyloP)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--phyloP",type=str,help='path to phyloP bigwig')
+parser.add_argument('--GTF', type=str,help='path to gtf file')
+parser.add_argument('--hits', type=str,help='path to pickle file of seekr hits')
+parser.add_argument('-n', type=int, help='Number of processors,default = number cpus avail',
+                    default=multiprocessing.cpu_count()-1)
+parser.add_argument('-w', type=int, help='Window for tile size', default=1000)
+parser.add_argument(
+    '-s', type=int, help='How many bp to slide tiles', default=100)
+args = parser.parse_args()
+
+
+phyloP = bw.open(args.phyloP)
+
+mouseGTF = gtfparse.read_gtf(args.GTF)
+mouseGTF['gene_id'] = [i[:-2] for i in mouseGTF['gene_id']]
+hits = pickle.load(open(args.hits,'rb'))
+
+df = pd.DataFrame.from_dict(hits,orient='index')
+query_dict = defaultdict(list)
+for row in df.iterrows():
+    query_dict = defaultdict(list)
+    name = row[0]
+    data = row[1:][0][0]
+    for i in data:
+        query_dict[i[1]].append(i[0])
+    hits[name] = query_dict.copy()
+    query_dict.clear()
+
+parsedDf = pd.DataFrame.from_dict(hits,orient='index')
+
+queryFasta = Reader('./queries.fa')
+queryHeaders = queryFasta.get_headers()
+
+queryMap = dict(zip(list(range(13)),queryHeaders))
+parsedDf.rename(columns=queryMap,inplace=True)
+queryCons = {}
+
+for query in parsedDf:
+    seekrGenomicCoords = dSeekrCoords(parsedDf,args.w,args.s)
+    with multiprocessing.Pool(args.n) as pool:
+        ha = pool.starmap(getCons, product(
+            *[[list(seekrGenomicCoords.items())],[mouseGTF]))
+        merge = dict(ha)
+    queryCons[query] = merge
+    print(queryCons)
+    1/0
 
 
 df = pd.DataFrame([seekrAgg,intronsAgg,exonsAgg])
 df = df.T
 df.columns=['seekr','introns','exons']
 
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 sns.set_context('talk')
 plt.figure(figsize=(4,7))
